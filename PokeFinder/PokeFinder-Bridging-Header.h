ae9aa80d43315cb31963d335f1d9fe20ac10cf5f
Ruslan Akberov <ruslanakberov@gmail.com>
2017-11-26 15:38:55 +0300

    Firebase and Geofire imported

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Руслан Акберов on 26.11.2017.
+//  Copyright © 2017 Ruslan Akberov. All rights reserved.
+//
+
+#import "GeoFire.h"
